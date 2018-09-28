11402a0e0331f6e9b01c2187e094644c550b1e56
Igor Ivanov <drkierkegor@gmail.com>
2018-09-28 14:58:52 +0300

    sm updt

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-form></b-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'b-form': Form
   }
 }
 </script>
